28a41d1970a94e643e027a9b4ffcf70a160b232a
Clovandi <56771701+Clovandi@users.noreply.github.com>
2020-01-27 16:54:21 -0300

    Adicionei arquivos via upload

---
+++ funpaper/src/br/com/funpaper/git/HelloGit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package br.com.funpaper.git;
+
+/**
+ * @author André Clovandi
+ * Exemplo de Integração do Eclipse com o Git Hub
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ funpaper/src/module-info.java
@@ -0,0 +1,2 @@
+module funpaper {
+}
