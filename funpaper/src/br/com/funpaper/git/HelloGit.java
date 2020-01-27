f1fcd6e770c8043c765403939579d59f6e46555f
André Clovandi <Andr Clovandi@DESKTOP-KKI52QL>
2020-01-27 17:00:25 -0300

    Adicionei linha no código

--- funpaper/src/br/com/funpaper/git/HelloGit.java
+++ funpaper/src/br/com/funpaper/git/HelloGit.java
@@ -15,6 +15,7 @@ public class HelloGit {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		System.out.println("clovandi");
 		// TODO Auto-generated method stub
 
 	}
